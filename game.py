2339146528f70343e72b2515c12ac22dc7c00fc3
Talween Kaur <137745419+Talween@users.noreply.github.com>
2024-09-14 10:48:27 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-gam#   Author: Catherine Leung
+#   Author: Catherine Leung
 #   This is the game that you will code the bots to play.  You can also play against your bot
 #   To run the game you will need pygames installed.  See: https://pypi.org/project/pygame/
 #   Once you have pygames, you can run the game by using the command:
